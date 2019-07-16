02e3ea2afcac254b82d1e7c4056373787510771f
Tania Rascia <taniarascia@gmail.com>
2019-07-15 20:36:32 -0500

    fix rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -156,6 +156,7 @@ module.exports = {
               allMarkdownRemark(
                 limit: 1000,
                 sort: { order: DESC, fields: [fields___date] },
+                filter: { frontmatter: { template: { eq: "post" } } }
               ) {
                 edges {
                   node {
