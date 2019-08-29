9406b9ce97e6f29f26b8e92e4945d5cc7105e717
Tania Rascia <taniarascia@gmail.com>
2019-08-28 21:40:29 -0500

    new publication

--- data/publications.js
+++ data/publications.js
@@ -175,5 +175,9 @@ export default {
       title: 'The End of an Era: Migrating from WordPress to Gatsby',
       path: 'https://www.gatsbyjs.org/blog/2019-03-21-migrating-from-wordpress-to-gatsby/',
     },
+    {
+      title: 'Roll Your Own Comment System',
+      path: 'https://www.gatsbyjs.org/blog/2019-08-27-roll-your-own-comment-system/',
+    },
   ],
 }
