5d634c763d89db589e95149ce76fb4f8845c87e8
Tania Rascia <taniarascia@gmail.com>
2019-10-20 16:34:52 -0500

    Add TakeNote path

--- data/projects.js
+++ data/projects.js
@@ -2,6 +2,7 @@ export default [
   {
     icon: '📝',
     title: 'TakeNote',
+    path: 'https://takenote.dev',
     source: 'https://github.com/taniarascia/takenote',
     description: 'A web-based note-taking app with GitHub sync and Markdown support.',
   },
