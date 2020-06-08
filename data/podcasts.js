a1226ef2504911fd848367868f18eb7e53ba8518
Tania Rascia <taniarascia@gmail.com>
2020-06-07 22:59:42 -0500

    Add interview

--- data/podcasts.js
+++ data/podcasts.js
@@ -1,14 +1,21 @@
 import egghead from '../content/images/egghead.png'
 import hashnode from '../content/thumbnails/hashnode.svg'
+import sm from '../content/thumbnails/sm.png'
 
 export default [
   {
-    title: 'Switching Careers and Learning in Public - with Joel Hooks of Egghead.io',
+    title: 'The Secret Is Just Do a Little Bit Over a Long Time - with James King',
+    path:
+      'https://sitesmonster.com/interviews/tania-rascia-the-secret-is-just-do-a-little-bit-over-a-long-time',
+    img: sm,
+  },
+  {
+    title: 'Switching Careers and Learning in Public - with Joel Hooks of Egghead.io (Podcast)',
     path: 'https://egghead.io/podcasts/switching-careers-and-learning-in-public-with-tania-rascia',
     img: egghead,
   },
   {
-    title: 'Woman in Tech: Tania Rascia - with Bolaji Ayodeji of Hashnode',
+    title: 'With Bolaji Ayodeji of Hashnode',
     path: 'https://townhall.hashnode.com/women-in-tech-tania-rascia-ck2bfzn2100up3is10zhphol4',
     img: hashnode,
   },
