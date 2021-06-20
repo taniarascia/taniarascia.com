c50e9f1d99301328f25df1d31236235f7e245e93
Tania Rascia <taniarascia@gmail.com>
2021-06-20 16:19:07 -0500

    Fun button why not

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import { Link, graphql } from 'gatsby'
 import Helmet from 'react-helmet'
 
@@ -15,12 +15,27 @@ import interviews from '../data/interviews'
 import speaking from '../data/speaking'
 
 export default function BlogIndex({ data }) {
+  const [followers, setFollowers] = useState(0)
   const latest = data.latest.edges
   const popular = data.popular.edges
   const simplifiedLatest = useMemo(() => getSimplifiedPosts(latest), [latest])
   const simplifiedPopular = useMemo(() => getSimplifiedPosts(popular), [
     popular,
   ])
+
+  useEffect(() => {
+    async function getGithubAPI() {
+      // If you fork this and don't change this, I will find you
+      const response = await fetch('https://api.github.com/users/taniarascia')
+      const data = await response.json()
+
+      return data
+    }
+
+    getGithubAPI().then((data) => {
+      setFollowers(data.followers)
+    })
+  }, [])
 
   const Section = ({ title, children, button, ...props }) => (
     <section {...props}>
@@ -46,6 +61,29 @@ export default function BlogIndex({ data }) {
           &mdash; a compendium of the things I've learned and created over the
           years.
         </p>
+        {followers && (
+          <p>
+            <a
+              className="button github-button"
+              href="https://github.com/taniarascia"
+              style={{ display: 'inline-flex' }}
+            >
+              <svg
+                viewBox="0 0 16 16"
+                width="20"
+                height="20"
+                aria-hidden="true"
+                style={{ marginRight: '0.75rem' }}
+              >
+                <path
+                  fill="white"
+                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+                ></path>
+              </svg>
+              {`${followers.toLocaleString()} following`}
+            </a>
+          </p>
+        )}
       </Blurb>
       <div className="container index">
         <Section title="Latest Articles." button>
