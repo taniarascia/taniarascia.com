648013ca253c7fe44d2cf1d79b562fd8d43d6f66
Tania Rascia <taniarascia@gmail.com>
2021-09-02 01:57:43 -0500

    Right path

--- gatsby-config.js
+++ gatsby-config.js
@@ -104,7 +104,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'posts',
-        path: `${__dirname}/taniarascia-content/`,
+        path: `${__dirname}/content/`,
       },
     },
     {
