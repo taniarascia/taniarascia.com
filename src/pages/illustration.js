d6a083b3d9feb65b6f35db5207216939ff4f2a5d
Tania Rascia <hello@taniarascia.com>
2022-11-26 16:14:54 -0600

    Two new drawings

--- src/pages/illustration.js
+++ src/pages/illustration.js
@@ -7,12 +7,14 @@ import { Hero } from '../components/Hero'
 import config from '../utils/config'
 
 const images = [
+  { url: '/illustrations/Ram.png', title: 'RAM Ram' },
+  { url: '/illustrations/Kohaku.png', title: 'Kohaku River' },
   { url: '/illustrations/Slurms.png', title: 'Slurms MacKenzie' },
+  { url: '/illustrations/Charizard.png', title: 'Charizard' },
+  { url: '/illustrations/MechaRam.png', title: 'Mecha Ram' },
   { url: '/illustrations/Squirtle.png', title: 'Squirtle' },
   { url: '/illustrations/Wartortle.png', title: 'Wartortle' },
   { url: '/illustrations/Blastoise.png', title: 'Blastoise' },
-  { url: '/illustrations/Ram.png', title: 'RAM Ram' },
-  { url: '/illustrations/MechaRam.png', title: 'Mecha Ram' },
   { url: '/illustrations/Oddish.png', title: 'Oddish' },
   { url: '/illustrations/Bulbasaur.png', title: 'Bulbasaur' },
 ]
